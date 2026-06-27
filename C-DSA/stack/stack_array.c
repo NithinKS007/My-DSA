ee0e45870e3b20b4c9b3e22f0582f31f94804eb3
NithinKS007 <nithinksndkm@gmail.com>
2026-06-27 20:20:01 +0530

    changed stack array implementation to struct based

--- C-DSA/stack/stack_array.c
+++ C-DSA/stack/stack_array.c
@@ -1,103 +1,110 @@
 // Implimentation of array using stack
+// LIFO = Last In, First Out
 
 #include <stdio.h>
+#define MAX 10
 
-int choice;
-int stack[10];
-int top = 0;
-int element;
+struct Stack
+{
+    int array[MAX];
+    int top;
+};
 
-void menu();
-void push();
-void pop();
-void show_elements();
+void menu(struct Stack *s);
+void push(struct Stack *s);
+void pop(struct Stack *s);
+void show_elements(struct Stack *s);
 
 int main()
 {
-    menu();
+    struct Stack s;
+
+    s.top = 0;
+
+    menu(&s);
+
     return 0;
 }
 
-void menu()
+void menu(struct Stack *s)
 {
+    int choice;
+
     while (1)
     {
-        printf("\nEnter one of the following options:\n");
+        printf("\n===== STACK MENU =====\n");
         printf("1. PUSH\n");
         printf("2. POP\n");
         printf("3. SHOW ELEMENTS\n");
         printf("4. EXIT\n");
-
+        printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        if (choice == 1)
+        switch (choice)
         {
-            push();
-        }
-        else if (choice == 2)
-        {
-            pop();
-        }
-        else if (choice == 3)
-        {
-            show_elements();
-        }
-        else if (choice == 4)
-        {
-            printf("Exiting...\n");
+        case 1:
+            push(s);
             break;
-        }
-        else
-        {
+
+        case 2:
+            pop(s);
+            break;
+
+        case 3:
+            show_elements(s);
+            break;
+
+        case 4:
+            printf("Exiting...\n");
+            return;
+
+        default:
             printf("Invalid Choice!\n");
         }
     }
 }
-
-void push()
+void push(struct Stack *s)
 {
-    if (top < 10)
+    int element;
+
+    if (s->top == MAX)
     {
-        printf("Enter the element to be pushed to stack:\n");
-        scanf("%d", &element);
-        stack[top] = element;
-        top++;
+        printf("Stack Overflow!\n");
+        return;
     }
-    else
-    {
-        printf("Stack is full\n");
-    }
-    return;
+
+    printf("Enter element: ");
+    scanf("%d", &element);
+
+    s->array[s->top] = element;
+    s->top++;
 }
 
-void pop()
+void pop(struct Stack *s)
 {
-    if (top > 0)
+    if (s->top == 0)
     {
-        top--;
-        element = stack[top];
-        printf("Popped element:%d\n", element);
+        printf("Stack Underflow!\n");
+        return;
     }
-    else
-    {
-        printf("Stack is empty\n");
-    }
-    return;
+
+    s->top--;
+
+    printf("Popped Element: %d\n", s->array[s->top]);
 }
 
-void show_elements()
+void show_elements(struct Stack *s)
 {
-    if (top == 0)
+    if (s->top == 0)
     {
-        printf("Stack is Empty.\n");
+        printf("Stack is Empty!\n");
+        return;
     }
-    else
-    {
-        printf("Stack Elements:\n");
 
-        for (int i = 0; i < top; i++)
-        {
-            printf("%d\n", stack[i]);
-        }
+    printf("\nStack Elements:\n");
+
+    for (int i = s->top - 1; i >= 0; i--)
+    {
+        printf("%d\n", s->array[i]);
     }
 }
